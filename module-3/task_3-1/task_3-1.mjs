74b327c2635d69b9f76b11869b4d37c347f05fb0
Ella <ella@ahman.co>
2025-10-10 18:01:37 +0200

    course commit

--- module-3/task_3-1/task_3-1.mjs
+++ module-3/task_3-1/task_3-1.mjs
@@ -2,41 +2,111 @@
 import { printOut, newLine } from "../../common/script/utils.mjs";
 
 printOut("--- Part 1, 2, 3 ----------------------------------------------------------------------------------------");
-/* Put your code below here!*/
-printOut("Task 1, 2 and 3");
-let wakeUpTime = 2;
-if ("Replace this") {
+
+let wakeUpTime = 6;
+
+if (wakeUpTime === 7) {
   printOut("I can take the bus to school.");
+} else if (wakeUpTime === 8) {
+  printOut("I can take the train to school.");
+} else {
+  printOut("I have to take the car to school.");
 }
+
 printOut(newLine);
 
 printOut("--- Part 4, 5 --------------------------------------------------------------------------------------------");
-/* Put your code below here!*/
-printOut("Replace this with you answer!");
+
+let number = 0;
+
+if (number > 0) {
+  printOut("Positive");
+} else if (number < 0) {
+  printOut("Negative");
+} else {
+  printOut("Zero");
+}
+
 printOut(newLine);
 
-printOut("--- Part 6 ----------------------------------------------------------------------------------------------");
-/* Put your code below here!*/
-printOut("Replace this with you answer!");
-printOut(newLine);
+printOut("--- Part 6, Part 7 ----------------------------------------------------------------------------------------------");
 
-printOut("--- Part 7 ----------------------------------------------------------------------------------------------");
-/* Put your code below here!*/
-printOut("Replace this with you answer!");
+let imageSize = Math.floor(Math.random() * 8) + 1;
+
+printOut("Image size: " + imageSize + "MP");
+
+if (imageSize >= 6) {
+  printOut("Image is too large");
+} else if (imageSize >= 4) {
+  printOut("Thank you");
+} else {
+  printOut("The image is too small");
+}
+
 printOut(newLine);
 
 printOut("--- Part 8 ----------------------------------------------------------------------------------------------");
-/* Put your code below here!*/
-printOut("Replace this with you answer!");
+
+const monthList = [
+  "January", "February", "Mars", "April", "Mai",
+  "Juni", "Juli", "August", "September", "October", "November", "December"
+];
+
+const noOfMonth = monthList.length;
+
+const monthName = monthList[Math.floor(Math.random() * noOfMonth)];
+
+printOut("Month: " + monthName);
+
+if (monthName.includes("r")) {
+  printOut("You must take vitamin D");
+} else {
+  printOut("You do not need to take vitamin D");
+}
+
 printOut(newLine);
 
 printOut("--- Part 9 ----------------------------------------------------------------------------------------------");
-/* Put your code below here!*/
-printOut("Replace this with you answer!");
+
+
+const monthDays = {
+  "January": 31,
+  "February": 28, 
+  "Mars": 31,
+  "April": 30,
+  "Mai": 31,
+  "Jun": 30,
+  "Juli": 31,
+  "August": 31,
+  "September": 30,
+  "October": 31,
+  "November": 30,
+  "December": 31
+};
+
+
+printOut("Month: " + monthName);
+
+if (monthName.includes("r")) {
+  printOut("You must take vitamin D");
+} else {
+  printOut("You do not need to take vitamin D");
+}
+
+printOut("Number of days: " + monthDays[monthName]);
+
 printOut(newLine);
 
 /* Task 10*/
 printOut("--- Part 10 ---------------------------------------------------------------------------------------------");
-/* Put your code below here!*/
-printOut("Replace this with you answer!");
+
+if (monthName === "Mars" || monthName === "Mai") {
+  printOut("Gallery is closed");
+} else if (monthName === "April") {
+  printOut("Gallery open in temporary premises");
+} else {
+  printOut("Gallery is open");
+}
+
+
 printOut(newLine);
